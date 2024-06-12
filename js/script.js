e965a60d85d8e8c7f84e66c899e11d458f57266b
Faatima Abdirahmaan Ali <faatimaapdirahmaanali@gmail.com>
2024-06-12 09:20:47 +0300

    added service section

--- js/script.js
+++ js/script.js
@@ -3,3 +3,24 @@ window.addEventListener("scroll", function () {
   const header = this.document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 0);
 });
+// service section -- modal
+const servicemodals = document.querySelectorAll(".service-modal");
+const learnmorebtns = document.querySelectorAll(".learn-more-btn");
+const modalclosebtns = document.querySelectorAll(".modal-close-btn");
+
+var modal = function (modalclick) {
+  servicemodals[modalclick].classList.add("active");
+};
+learnmorebtns.forEach((learnmorebtn, i) => {
+  learnmorebtn.addEventListener("click", () => {
+    modal(i);
+  });
+});
+
+modalclosebtns.forEach((modalclosebtn) => {
+  modalclosebtn.addEventListener("click", () => {
+    servicemodals.forEach((modalview) => {
+      modalview.classList.remove("active");
+    });
+  });
+});
