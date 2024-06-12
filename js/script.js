e553c812abb7a84b457e9ad693a297970778a511
Faatima Abdirahmaan Ali <faatimaapdirahmaanali@gmail.com>
2024-06-12 20:47:51 +0300

    added contact and footer section also dark theme and scroll to top

--- js/script.js
+++ js/script.js
@@ -47,3 +47,73 @@ portfoliomodalclosebtns.forEach((portfoliomodalclosebtn) => {
     });
   });
 });
+// my clients
+// var swiper = new Swiper(".client-swiper", {
+//   slidesPreView: 1,
+//   spaceBetween: 30,
+//   loop: true,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
+
+// scroll to  top
+const scrollToTopBtn = document.querySelector(".scrollToTop-btn");
+window.addEventListener("scroll", function () {
+  scrollToTopBtn.classList.toggle("active", window.scrollY > 10);
+});
+scrollToTopBtn.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
+
+// // menu items active on page scroll
+// window.addEventListener("scroll", () => {
+// const sections=document.querySelectorAll("section")
+// const scrollY=window.pageYOffset;
+
+// sections.forEach(current=>{
+//   let sectionHeight=current.offsetHeight
+//   let sectionTop=current.offsetTop-50
+//   let id=current.getAttribute("id")
+
+//   if(scrollY>sectionTop&&scrollY<=sectionTop+sectionHeight){
+//     document.querySelector(".nav-items a[href*=" + id + "]").classList.add("active")
+//   }
+//   else{
+//     document.querySelector(".nav-items a[href*=" + id + "]").classList.remove("active")
+
+//   }
+// })
+// })
+
+// theme btn
+const themebtn = document.querySelector(".theme-btn");
+
+themebtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme");
+  themebtn.classList.toggle("sun");
+
+  localStorage.setItem("saved-theme", getCurrentTheme());
+  localStorage.setItem("saved-icon", getCurrentIcon());
+});
+
+const getCurrentTheme = () =>
+  document.classList.contains("dark-theme") ? "dark" : "light";
+const getCurrentIcon = () =>
+  document.classList.contains("sun") ? "sun" : "moon";
+
+const savedTheme = localStorage.getItem("saved-theme");
+const savedIcon = localStorage.getItem("saved-icon");
+
+if (savedTheme) {
+  document.body.classList[savedTheme === "dark" ? "add" : "remove"](
+    "dark-theme"
+  );
+  themebtn.classList[savedIcon === "sun" ? "add" : "remove"]("sun");
+}
